f341621b57b710b1cfb6f9a7570d6c7474433158
gem <111511069+gemvcnte@users.noreply.github.com>
2024-04-01 06:01:55 +0800

    Delete routes directory

--- routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const jwt = require("jsonwebtoken");
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/authController");
-
-router.post("/login", authController.adminLogin);
-
-module.exports = router;
