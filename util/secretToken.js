fe36c4282057d6a51631cf1bfa2850461c758356
gem <111511069+gemvcnte@users.noreply.github.com>
2024-04-01 06:02:04 +0800

    Delete util directory

--- util/secretToken.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require("dotenv");
-const jwt = require("jsonwebtoken");
-dotenv.config();
-
-module.exports.createSecretToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_KEY, {
-    expiresIn: 3 * 24 * 60 * 60,
-  });
-};
